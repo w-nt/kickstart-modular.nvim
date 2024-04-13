7b765b986531c64d801d36c9a01befe6ba6f9cda
W-ent <pessonthibault@gmail.com>
2024-04-14 01:49:11 +0200

    added keybinds for neotree

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -33,6 +33,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- NeoTree keybind to enable / disable the window
+vim.keymap.set('n', '<leader>t', ':Neotree toggle<CR>', { desc = 'Toggle Neo[T]ree' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
