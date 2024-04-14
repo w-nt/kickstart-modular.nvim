12ea858f861f9a947f321da12bfe2226f9123614
W-ent <pessonthibault@gmail.com>
2024-04-14 14:26:06 +0200

    fixed a bug that prevented objc file from formatting

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -1,3 +1,6 @@
+-- Set the filetype for .h files to objc, for some reason vim detects .h as cpp
+vim.cmd 'autocmd BufNewFile,BufRead *.h setfiletype objc'
+
 return {
   { -- Autoformat
     'stevearc/conform.nvim',
@@ -18,7 +21,7 @@ return {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = {}
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
@@ -29,15 +32,17 @@ return {
         -- Conform can also run multiple formatters sequentially
         python = { 'black' },
         c = { 'astyle' },
+        objc = { 'astyle' },
+        cpp = { 'astyle' },
 
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { 'prettier' },
       },
       formatters = {
         astyle = {
-          prepend_args = { '--style=allman', '--align-pointer=type' },
+          prepend_args = { '--style=allman', '--align-pointer=type', '--unpad-paren', '--align-reference=type' },
         },
       },
     },
