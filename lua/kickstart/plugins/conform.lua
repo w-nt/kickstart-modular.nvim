99fafc73d3fe6893b9d1d900e58d7bf032bb190e
W-ent <pessonthibault@gmail.com>
2024-04-14 02:45:28 +0200

    added c and py formatters

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -27,7 +27,8 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'black' },
+        c = { 'astyle' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
